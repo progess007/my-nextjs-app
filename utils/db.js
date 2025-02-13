8740c2e7b350b868b107115b48dddcdc769fbbc1
progess007 <karn.sa.62@ubu.ac.th>
2025-02-13 15:38:25 +0700

    Initial commit

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise'
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
+  host: process.env.DB_HOST || '203.149.8.14',
+  user: process.env.DB_USER || 'karn',
   password: process.env.DB_PASS || 'Admin@123',
   database: process.env.DB_NAME || 'recommend_system',
 })
